3d0a4ff058d86ee3c9d1d2e5ef1813f8f6148edf
Mikhail <michaildrazdovich@gmail.com>
2020-12-06 23:46:54 +0300

    Fix

--- container/src/App.js
+++ container/src/App.js
@@ -2,7 +2,9 @@ import React from 'react';
 import MarketingApp from './components/MarketingApp';
 
 const App = () => {
-    return <MarketingApp/>;
+    return (
+        <MarketingApp/>
+    );
 };
 
 export default App;
